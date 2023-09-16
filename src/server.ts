aec20123c26b3566753f4790f2691b025d4370ed
Hashib Raja <108208385+hashib786@users.noreply.github.com>
2023-09-16 19:29:18 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req : Request, res : Response)=>{
     res.status(200).json({
-        message : "Hello World!"
+        message : "Hello World!!!!!"
     })
 })
 
